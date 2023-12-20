a4f09ffba8337de6695861cef9d2b4ce877d2779
CalvinKuan <91074355+CalvinKuan@users.noreply.github.com>
2023-12-20 17:13:28 +0800

    update upload by CK

--- WEB-INF/classes/ncu/im3069/demo/controller/UploadController.java
+++ WEB-INF/classes/ncu/im3069/demo/controller/UploadController.java
@@ -1,8 +1,11 @@
 package ncu.im3069.demo.controller;
 
-import java.io.*;
-import java.util.Collection;
-import java.util.Enumeration;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -14,61 +17,42 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
-import com.oreilly.servlet.MultipartRequest;
-
-/**
- * Servlet implementation class uploadController
- */
 @WebServlet("/api/upload.do")
-@MultipartConfig
+@MultipartConfig(
+	    fileSizeThreshold = 1024 * 1024 * 2,  // 2 MB
+	    maxFileSize = 1024 * 1024 * 10,         // 10 MB
+	    maxRequestSize = 1024 * 1024 * 50       // 50 MB
+	)
+
 public class UploadController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-      
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public UploadController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        try {
+            ServletFileUpload sf = new ServletFileUpload(new DiskFileItemFactory());
+            List<FileItem> multifiles = sf.parseRequest(request);
+            System.out.println(request);
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		try {
-			ServletFileUpload sf = new ServletFileUpload(new DiskFileItemFactory());
-			List<FileItem> multifiles = sf.parseRequest(request);
-			System.out.println(request);
-			
-			String cla = this.getClass().getClassLoader().getResource(".").getPath();
-			String img_path = cla.replace("/WEB-INF/classes/", "/img/");
-			//System.out.println(cla);
-			
-			for(FileItem item:multifiles) {
-				
-				item.write(new File(img_path+item.getName()));
-				System.out.println(img_path+item.getName());
-			}
-			System.out.println("Upload Success!");
-			}catch(Exception ex){
-				System.out.println(ex);
-				System.out.println("Upload fail!");
-			}
-		
-		}
-   
+            //String cla = this.getClass().getClassLoader().getResource(".").getPath();
+            //String img_path = cla.replace("/WEB-INF/classes/", "/img/");
+            //System.out.println(cla);
+
+            for(FileItem item:multifiles) {
+
+                item.write(new File("112_SA_project_G2/img/"+item.getName()));
+                System.out.println(item.getName());
+            }
+            System.out.println("Upload Success!");
+            }catch(Exception ex){
+                System.out.println(ex);
+                System.out.println("Upload fail!");
+            }
+
+        }
+
+    
 }
